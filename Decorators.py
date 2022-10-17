385ba7844459396bf798c5766f2bb0d4733c6c68
=Steavo <=steavobabu638@gmail.com>
2022-10-17 10:58:33 +0530

    Added comments

--- Decorators.py
+++ Decorators.py
@@ -1,8 +1,8 @@
-def div (a,b):                    #original function
+def div (a,b):                   #original function to divide two numbers
     print(a/b)
 
 
-def smart_div(func):            #decorator
+def smart_div(func):            #decorator #function are being passed as an argument as functions are an object in python
     def inner(a,b):                         #new function which modifies the existing function
         if a<b:
             a,b=b,a
@@ -11,4 +11,5 @@ def smart_div(func):            #decorator
 
 
 div = smart_div(div)         #assigning 2 functions (new function and old function)
-div(2,4)    #calling the modiefied new function
+div(2,4)    #calling the modified new function
+# Code by Steavo Babu
--- for_else.py
+++ for_else.py
@@ -1,6 +1,6 @@
 #for else in python
 
-nums=[14,34,43,34,56]
+nums=[14,34,43,34,56]           #created a list
 
 for num in nums :
     if num%5==0:
@@ -8,5 +8,6 @@ for num in nums :
         break   #break is compulsory
 else:           #this else doesnt comes under if statement , but comes under for loop
     print("not found")  #if no number is found in the list so instead of iterating over the if statement
-                                #else statement can be used with respect to for
-                                
+                                #else statement can be used with respect to : 'for'
+
+# Code by Steavo Babu
