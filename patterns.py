23b0477d971c8bf2f9ce14d11d8a12f5eb79ba55
=Steavo <=steavobabu638@gmail.com>
2022-10-20 19:09:29 +0530

    updated patterns

--- patterns.py
+++ patterns.py
@@ -1,19 +1,19 @@
-'''
+
 for r in range(5):
     for c in range(r):
-        print("#",end=" ") #end =" " is used such that the control wont go to the next line
-    print() #used for getting into new line // print("\n") can also be used 
-'''
-'''
+        print("#",end=" ")   #end =" " is used such that the control wont go to the next line
+    print()  #used for getting into new line // print("\n") can also be used 
+
+
 for r in range(4):
     for c in range(r+1):
-        print("#",end=" ") #end =" " is used such that the control wont go to the next line
-    print() #used for getting into new line // print("\n") can also be used
-'''
-'''
-for r in range(4,0,-1):           #range(4)
-    for c in range(r):              # range(4-r)s
-        print("#",end=" ") #end =" " is used such that the control wont go to the next line
-    print() #used for getting into new line // print("\n") can also be used 
+        print("#",end=" ") 
+    print() 
 
-'''
+
+for r in range(4,0,-1):           
+    for c in range(r):              
+        print("#",end=" ") 
+    print()
+
+#Code by Steavo Babu
