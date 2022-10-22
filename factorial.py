314f8d0dbbb381a7b6a25bb7ef29cf81f036f7cd
=Steavo <=74451045+Steavo-git@users.noreply.github.com>
2022-10-22 22:31:34 +0530

    modified code

--- factorial.py
+++ factorial.py
@@ -1,15 +1,17 @@
+#to print the factorial of a given number
+def factorial(n):
+    
+        fact=1
+        for i in range(1,n+1):          #for looping 
+            fact = fact*i
+          
+        return fact                     #returning the final value
 
+num=int(input("Enter the number :"))
+if num<0:                                                   #for negative integers
+    print("Please enter a positive integer!")           
+else:
+    fact=factorial(num)
+    print("Factorial of ",num, "is ",fact)
 
-def fact(n):
-    fact=1
-    for i in range(1,n+1):
-        fact = fact*i
-      # i+=1
-
-    return fact
-
-
-
-
-res=fact(5)
-print(res)
+#Code by Steavo Babu
