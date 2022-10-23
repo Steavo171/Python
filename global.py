8847277143a6088250b6d851255fd498d4a29df4
=Steavo <=74451045+Steavo-git@users.noreply.github.com>
2022-10-23 09:41:22 +0530

    Updated codes

--- global.py
+++ global.py
@@ -1,23 +1,23 @@
+#to demonstrate the working to global variables
 a=10
 b=7
 c=5
 f=0
-def dd():
+def sample():
     x=2
 
-fh=globals()['a']  #will refer to all the global variables in the code unless specified by []
-print(fh)
+f=globals()['a']  # 'globals' will refer to all the global variables in the code unless specified by []
+print(f)        #will print 10
 
 
-'''
 a=10
 def something():
-    global a
+    global a         #a variable in a function can be converted from local to global using global keyword
     a=15
-    #a=15
     print("in function",a)
 
 something()
 
 print("outside",a)
-'''
+
+#Code by Steavo Babu 
