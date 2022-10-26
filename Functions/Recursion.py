1b5406a624f40d0e5b0e94a814363d02dd4afa64
Steavo-git <=steavobabu638@gmail.com>
2022-10-26 20:52:09 +0530

    defined recursion

--- Functions/Recursion.py
+++ Functions/Recursion.py
@@ -1,10 +1,19 @@
-import sys
+# Recursion - Recursion refers to the calling of same function by itself
+
+import sys      #sys is a module
+print(sys.getrecursionlimit())          #this function is used to determine the recursion limit 
+                                                        #by default recursion limit is 1000 
+
+sys.setrecursionlimit(1001)             #this function is used to change/set the recursion limit
+
 print(sys.getrecursionlimit())
-'''
-def greet():
+
+
+def greet():    #function declared
+    
     print("Hello")
-    greet()
+    greet()         #function being called by itself in the scope of the same function
 
 greet()
 
-'''
+#Code by Steavo Babu
