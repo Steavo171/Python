419819390a237a76082ad6a047bcb751ef9df9d2
Steavo-git <steavobabu638@gmail.com>
2022-11-01 00:05:49 +0530

    added comments

---
+++ Numpy.py
@@ -0,0 +1,11 @@
+from numpy import *
+arr=linspace(1,15,3)   #by using linspace function
+                                      #takes three parameters start stop
+print(arr)
+arr=linspace(0,5,6)     #numpy linspace returns evenly spaced numbers over a specified interval. 
+print(arr)
+arr=logspace(0,4,2)     #numpy logspace return numbers spaced evenly on a log scale
+print(arr)
+print('%.2f' %arr[1])
+
+#Code by Steavo Babu
